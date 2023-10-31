abeb2cffe0555de8772fe27fad1f78b3eb78db20
Rolando Chirinos <yutipu@gmail.com>
2023-10-30 22:39:29 -0500

    primer avance

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(indexRouter)
+app.use(express.static(join(__dirname, "public")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => res.render("index"));
-router.get("/about", (req, res) => res.render("about"));
-router.get("/contact", (req, res) => res.render("contact"));
+router.get("/", (req, res) => res.render("index",{title:'HomePage'}));
+router.get("/about", (req, res) => res.render("about",{title:'About me'}));
+router.get("/contact", (req, res) => res.render("contact",{title:'Contact me'}));
 
 export default router;
