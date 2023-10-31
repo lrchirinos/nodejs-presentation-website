f8257c4a3b42f7cf6895c88b033b9cab4f8486e2
Rolando Chirinos <yutipu@gmail.com>
2023-10-30 20:07:21 -0500

    ruteos

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import ejs from "ejs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import indexRouter from './routes/index.js'
+
+
+const app = express();
+const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(indexRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
